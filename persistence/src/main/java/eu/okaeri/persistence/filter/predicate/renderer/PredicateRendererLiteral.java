d0f5f83ecf1849b2533bb9157202b2fb0bb7c995
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-05-04 07:23:25 +0200

    Allow passing any literal string to predicate renderer

---
+++ persistence/src/main/java/eu/okaeri/persistence/filter/predicate/renderer/PredicateRendererLiteral.java
@@ -0,0 +1,10 @@
+package eu.okaeri.persistence.filter.predicate.renderer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PredicateRendererLiteral {
+    private String value;
+}
